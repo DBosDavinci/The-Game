1af2e1a92fef35f147c7b24775d9dbad0e98ae3a
DBosDavinci <99069008@mydavinci.nl>
2021-09-10 14:31:37 +0200

    Start

---
+++ The-Game.py
@@ -0,0 +1,11 @@
+# The Game
+
+name = input("Welcome to The Game Zombie apocalypse, please choose your name!")
+print("The objective of this game is to reach the airport, which is close to the forest outside of the city. Once you get there you will escape from the zombie infected city.")
+
+keuze = input(name,"wakes up in an empty factory, he hears some growling. He turns around and sees a zombie at the entrance of the factory. What will he do?\n"
+"a) Run for it\n"
+"b) Looks for a weapon to slaughter it")
+
+if keuze == "A" or "a":
+    keuze1 = input("")
