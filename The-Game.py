ece75ffe6fd8672c29b3553dbe4c49ac134688c2
DBosDavinci <99069008@mydavinci.nl>
2021-09-13 11:51:46 +0200

    aantal keuzes af

--- The-Game.py
+++ The-Game.py
@@ -1,11 +1,90 @@
 # The Game
 
 name = input("Welcome to The Game Zombie apocalypse, please choose your name!")
-print("The objective of this game is to reach the airport, which is close to the forest outside of the city. Once you get there you will escape from the zombie infected city.")
+print("The objective of this game is to reach the airport, which is close to the forest outside of the city. Once you get there you can escape from the zombie infected city.")
 
-keuze = input(name,"wakes up in an empty factory, he hears some growling. He turns around and sees a zombie at the entrance of the factory. What will he do?\n"
+keuze1 = input(name,"wakes up in an empty factory, he hears some growling. He turns around and sees a zombie at the entrance of the factory. What will he do?\n"
 "a) Run for it\n"
-"b) Looks for a weapon to slaughter it")
+"b) Look for a weapon to slaughter it")
 
-if keuze == "A" or "a":
-    keuze1 = input("")
+if keuze1 == "A" or "a":
+    keuze2 = input(name,"chose to run for it. They walk outside and see a forest close by and a hospital on the opposite side of the road. Where will",name,"go?\n"
+    "a) Go to the nearby forest\n"
+    "b) Go to the hospital on the opposite side of road")
+    if keuze2 == "A" or "a":
+        keuze3 = input(name,"arrives in the forest, you arrive at an abandoned house. Will you go inside or go further into the forest?\n"
+        "a) Enter the abandoned house\n"
+        "b) Go deeper into the forest?")
+        if keuze3 == "a" or "A":
+            keuze4 = input()
+            if keuze4 == "a" or "A":
+                keuze5 = input()
+                if keuze5 == "a" or "A":
+                    keuze6 = input()
+                    if keuze6 == "a" or "A":
+                        keuze7 = input()
+                        if keuze7 == "a" or "A":
+                            print(name,"chooses to go left")
+                        if keuze7 == "b" or "B":
+                            keuze8 = input()
+                            if keuze8 == "a" or "A":
+                                keuze9 = input()
+                                if keuze9 == "a" or "A":
+                                    print()
+                                if keuze9 == "b" or "B":
+                                    keuze10 = input()
+                                    if keuze10 == "a" or "B":
+                                        print("You walk to the man on the airport, just before you can open your mouth to speak to him you notice he has a weird look on his face. The man walks straight towards you and you try"
+                                        "to run from him, but there is already a swarm of zombies behind you. RIP")
+                                    if keuze10 == "b" or "B":
+                                        print("You walk to the airplane, you drive onto the flying path. A swarm of zombies is coming after you, but you are out of reach. You fly away and you completed the game! Congratulations",name,"!")
+                            if keuze8 == "b" or "B":
+                                print()                   
+                    if keuze6 == "b" or "B":
+                        input()                
+                if keuze5 == "b" or "B":
+                    keuze6 = input("In the house you encounter a zombie, will you attack it or run from it?\n"
+                    "a) Attack the zombie"
+                    "b) Run from the zombie")
+                    if keuze6 == "a" or "A":
+                        print("You try to attack the zombie, but you fail to swing at the zombie because you don't have enough energy left.")
+                    if keuze6 == "b" or "B":
+                        print("You try to run from the zombie, but you don't have enough energy left. RIP")
+    if keuze2 == "A" or "a":
+        keuze3 = input()
+if keuze1 == "b" or "B":
+    keuze2 = input("You find a broken glass bottle on the floor, you can use to try to kill the zombie. Will you try it?\n"
+    "a) Attack the zombie\n"
+    "b) Look for more weapons outside")
+    if keuze2 == "a" or "A":
+        print(name,"tries to kill the zombie, you succeed, but while you were killing it 5 other zombies entered the building. RIP")
+    if keuze2 == "b" or "B":
+        keuze3 = input()
+        if keuze3 == "a" or "A":
+            keuze4 = input()
+            if keuze4 == "a" or "A":
+                keuze5 = input()
+                if keuze5 == "a" or "A":
+                    print()
+                if keuze5 == "b" or "B":
+                    keuze6 = input()
+                    if keuze6 == "a" or "A":
+                        keuze7 = input()
+                        if keuze7 == "a" or "A":
+                            keuze8 = input()
+                            if keuze8 == "a" or "A":
+                                print()
+                            if keuze8 == "b" or "B":
+                                keuze9 = input()
+                                if keuze9 == "a" or "A":
+                                    print()
+                                if keuze9 == "b" or "B":
+                                    print()
+                        if keuze7 == "b" or "B":
+                            print()
+                    if keuze6 == "b" or "B":
+                        print()
+            if keuze4 == "b" or "B":
+                print()
+        if keuze3 == "b" or "B":
+            print()
